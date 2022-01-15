dcf3a4f54776b4d4a705ee036fc0e22666574039
LovishPrajapati <prajapatilovish@gmail.com>
2022-01-15 20:00:05 +0530

    fixed

--- pages/api/rooms/index.js
+++ pages/api/rooms/index.js
@@ -2,7 +2,7 @@ import { allRooms, newRoom } from "../../../controller/roomController";
 import nc from "next-connect";
 import dbConnect from "../../../congif/dbConnect";
 const handler = nc();
-dbConnect()
-// handler.get(allRooms);
-handler.post(newRoom)
+dbConnect();
+handler.get(allRooms);
+handler.post(newRoom);
 export default handler;
